1d6c1a65d4865ffbc99b805b4d19d8f03a953504
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-01-29 17:53:14 +0000

    add sheet 3 plus crappy proof

---
+++ src/section06orderings_and_lattices/sheet3.lean
@@ -0,0 +1,31 @@
+/-
+Copyright (c) 2023 Kevin Buzzard. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author : Kevin Buzzard
+-/
+
+import tactic
+
+/-
+
+# A harder question about lattices
+
+I learnt this fact when preparing sheet 2.
+
+With sets we have `A ∪ (B ∩ C) = (A ∪ B) ∩ (A ∪ C)`, and `A ∩ (B ∪ C) = (A ∩ B) ∪ (A ∩ C)`.
+In sheet 2 we saw an explicit example (the lattice of subspaces of a 2-d vector space)
+of a lattice where neither `A ⊓ (B ⊔ C) = (A ⊔ B) ⊓ (A ⊔ C)` nor `A ⊓ (B ⊔ C) = (A ⊓ B) ⊔ (A ⊓ C)`
+held. But it turns out that in a general lattice, one equality holds if and only if the
+other one does! This was quite surprising to me.
+
+The challenge is to prove it in Lean. My strategy would be to prove it on paper first
+and then formalise the proof. If you're not in to puzzles like this, then feel free to skip
+this question. 
+
+-/
+
+example (L : Type) [lattice L] : 
+  (∀ a b c : L, a ⊔ (b ⊓ c) = (a ⊔ b) ⊓ (a ⊔ c)) ↔ (∀ a b c : L, a ⊓ (b ⊔ c) = (a ⊓ b) ⊔ (a ⊓ c)) :=
+begin
+  sorry,
+end
