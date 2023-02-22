0a86a3d0dc2f11198b24ff5c99b408a39498a15f
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-02-22 11:09:01 +0000

    measure theory sheets 2 and 3

---
+++ src/section12measure_theory/sheet2.lean
@@ -0,0 +1,53 @@
+/-
+Copyright (c) 2023 Kevin Buzzard. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author : Kevin Buzzard
+-/
+
+import tactic -- imports all the Lean tactics
+import measure_theory.measurable_space
+
+/-
+
+# Measure theory
+
+## More on sigma algebras.
+
+-/
+
+-- Intersection of sigma algebras is a sigma algebra
+
+-- Let 𝓐 be a family of sigma algebras on a type `X`
+variables (X : Type) (I : Type) (𝓐 : I → measurable_space X)
+
+-- Then their intersection is also a sigma algebra
+
+open_locale measure_theory -- to get notation `measurable_set[S] U` 
+-- for "U is in the sigma algebra S"
+
+example : measurable_space X :=
+{ measurable_set' := λ U, ∀ i : I, measurable_set[𝓐 i] U,
+  measurable_set_empty := begin
+    sorry
+  end,
+  measurable_set_compl := begin
+    sorry
+  end,
+  measurable_set_Union := begin
+    sorry
+  end }
+
+-- Lean knows that sigma algebras on X are a complete lattice
+-- so you could also make it like this:
+example : measurable_space X := ⨅ i, 𝓐 i
+
+-- Sigma algebras are closed under countable intersection
+-- Here, because there's only one sigma algebra involved,
+-- I use the typeclass inference system to say "fix a canonical
+-- sigma algebra on X" and just use that one throughout the question.
+example (X : Type) [measurable_space X] (f : ℕ → set X)
+  (hf : ∀ n, measurable_set (f n)) : measurable_set (⋂ n, f n) :=
+begin
+  sorry
+end
+
