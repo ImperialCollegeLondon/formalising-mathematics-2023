5c10ed33e710c75a836dac435992f18b26374894
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-03-08 14:51:38 +0000

    add hints

---
+++ src/solutions/section16curves_and_surfaces/sheet1.lean
@@ -0,0 +1,22 @@
+/-
+Copyright (c) 2023 Kevin Buzzard. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author : Kevin Buzzard
+-/
+
+import tactic
+import data.real.basic
+import analysis.calculus.parametric_integral
+
+/-
+
+# Curves and surfaces
+
+Let's see how feasible this is!
+
+-/
+
+noncomputable def φ₁ : ℝ → ℝ × ℝ := 
+λ x, (real.cos x, real.sin x)
+
+example : cont_diff_on ℝ ⊤ φ₁ (set.Icc 0 1) := sorry 
